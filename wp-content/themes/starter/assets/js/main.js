14142c06c4bb7c46d898c46ff2e904cae4157748
Mike Smith <mikes@thisismc2.com>
2015-01-22 13:14:47 +0000

    tidy main.js

--- wp-content/themes/starter/assets/js/main.js
+++ wp-content/themes/starter/assets/js/main.js
@@ -26,29 +26,6 @@ $(document).ready(function() {
     });
 */
 
-
-/*
-
-    Fixed Header
-
-*/
-    var fixed;
-    
-    // function to make header position:fixed; after a certain height is scrolled:
-    function fixedHeader(ev){
-        scrolled = $(window).scrollTop();
-        if(scrolled > 100 && !fixed){
-            $('.page-header').fadeOut(300,function(){
-                $('.header-spacer').css('height',headerHeight)
-                $(this).css('position','fixed').fadeIn(300);
-                $('.page-header__hamburger').css('background','rgba(0,0,0,0.3)');
-            });
-           fixed = true;
-        }
-    } // /fixedHeader
-
-    $(window).scroll(fixedHeader); // repeatedly execute whenever page is scrolled
-
 /*
 
     Mobile Nav show/hide animation
@@ -62,18 +39,6 @@ $(document).ready(function() {
         $('.page-header__nav--mobile').slideToggle(function(e){
             $('.page-header__hamburger .open, .page-header__hamburger .close').toggle();
         });
-    });
-
-/*
-
-    Expanding Content Area (Home Page)
-
-*/
-    $('.expand + .hidden-content').hide(); // hide if jquery works
-
-    $('.expand').on('click', function(event) {
-        $(this).toggleClass('focus__expand--expanded');
-        $(this).next('.hidden-content').slideToggle();
     });
 
 /*
